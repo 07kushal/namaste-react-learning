adaba7151c9b7b228af2b733fe371a1f836802b7
07kushal <saini.kushal2@gmail.com>
2023-05-08 23:16:42 +0530

    Filter Top Restaurants button added. Episode 5 done

--- src/component/Restaurants.js
+++ src/component/Restaurants.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { restaurantsData } from "../Config/Data";
 import RestaurantCard from "./RestaurantCard";
 
 const Restaurants = () => {
-  console.log("restaurants", restaurantsData);
+  const [listOfRestaurants, setListOfRestaurants] = useState([...restaurantsData]);
+  const onClickFilter = () => {
+    const filterList = listOfRestaurants.filter((res) => res?.avgRating > 4);
+    setListOfRestaurants([...filterList]);
+  };
   return (
     <div className="restaurants-container">
       <h2 className="list-title">Restaurants</h2>
+      <button className="filter-button" onClick={onClickFilter}>
+        Filter Top Restaurants
+      </button>
       <div className="restaurants-list-container">
-        {restaurantsData.map((restaurant) => {
+        {/* not using keys (not acceptable) << index as key <<<< unique key (best practices) */}
+        {listOfRestaurants.map((restaurant) => {
           return <RestaurantCard key={restaurant.id} {...restaurant} />;
         })}
       </div>
